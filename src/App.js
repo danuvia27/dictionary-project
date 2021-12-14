743ac640c53e81937bd506f2055b5e50ac2dd960
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-12-14 11:22:21 -0800

    created form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from "./logo-small.jpeg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Carolina Vallecillo</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
