bf405d49d538b1bd272edc748adba9747d4741f6
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-12-20 13:25:30 -0800

    created synonyms.js arrays

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,7 +8,17 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <strong>Definition:</strong>
+            <p>
+              {definition.definition}
+              <br />
+              <strong>Example:</strong>
+              <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+              <strong>Synonyms:</strong>
+              {definition.synonyms}
+            </p>
           </div>
         );
       })}
